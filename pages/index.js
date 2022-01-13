8b8853d1976fc4f01a73cfe9739ecc6433ce8973
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-01-13 09:49:56 +0200

    Rendering A List Of (Dummy) Meetups

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
 import React from "react";
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "First meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/7/7f/Kolonaki_Square_2.jpg",
+    address: "Some address 5, Kolonaki",
+    description: "Our first meetup",
+  },
+  {
+    id: "m2",
+    title: "Second meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/f/fe/%CE%A0%CE%BB%CE%AC%CE%BA%CE%B1_6552.jpg",
+    address: "Some address 6, Plaka",
+    description: "Our second meetup",
+  },
+];
 
 const HomePage = () => {
-  return <div></div>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 };
 
 export default HomePage;
