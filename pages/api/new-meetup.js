fac6e2584e0e09658f9eb964199523d1b032c991
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-01-15 14:45:18 +0200

    Connecting & Querying a MongoDB Database

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,8 +1,21 @@
+import { MongoClient } from "mongodb";
+
 const handler = async (req, res) => {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, description, address } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://NEXTJSUSER:aiD0ppjwUKorq8pB@cluster0.tkycs.mongodb.net/meetupsData?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted" });
   }
 };
 
