d9b953aa328250ae86140bef2dcd739a822f726a
Philip Alexander Hassialis <philip.alexander.hassialis@gmail.com>
2022-01-16 00:49:49 +0200

    Getting Data From The Database (For Page Pre-Rendering)

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { MongoClient } from "mongodb";
 import React, { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
@@ -24,11 +25,28 @@ const HomePage = (props) => {
   return <MeetupList meetups={props.meetups} />;
 };
 
-export const getStaticProps = (context) => {
+export const getStaticProps = async (context) => {
   // fetch data from somewhere
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://NEXTJSUSER:aiD0ppjwUKorq8pB@cluster0.tkycs.mongodb.net/meetupsData?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
     revalidate: 10,
   };
